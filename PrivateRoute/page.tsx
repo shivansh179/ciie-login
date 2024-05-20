0c1ff711761ee72f098b45d60740bc61bfed00a3
Shivansh Shukla <prasantshukla89@gmail.com>
2024-05-21 01:09:08 +0530

    Fixed some security patches

--- PrivateRoute/page.tsx
+++ PrivateRoute/page.tsx
@@ -1,23 +1,20 @@
-'use client';
-
-// components/PublicRoute.js
+// components/PrivateRoute.js
+import { ReactNode, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebaseConfig';
-import { ReactNode } from 'react';
 
-interface PublicRouteProps {
+interface PrivateRouteProps {
   children: ReactNode;
 }
 
-const PublicRoute = ({ children }: PublicRouteProps) => {
-  const [user, loading] = useAuthState(auth);
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+  const [user, loading, error] = useAuthState(auth);
   const router = useRouter();
 
   useEffect(() => {
-    if (!loading && user) {
-      router.push('/Dashboard');
+    if (!loading && !user) {
+      router.push('/Login');
     }
   }, [user, loading, router]);
 
@@ -25,11 +22,11 @@ const PublicRoute = ({ children }: PublicRouteProps) => {
     return <div>Loading...</div>; // You can render a loading spinner here
   }
 
-  if (!user) {
-    return <>{children}</>;
+  if (user) {
+    return <>{children}</>; // Ensure children are rendered properly
   }
 
   return null;
 };
 
-export default PublicRoute;
+export default PrivateRoute;
